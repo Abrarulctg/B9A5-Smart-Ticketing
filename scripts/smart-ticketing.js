c184510287c6dbdd87ea90cfce8ddd2479ec99c8
Abrarulctg <jubairctg99@gmail.com>
2024-02-20 13:10:05 +0600

    calculated total price

--- scripts/smart-ticketing.js
+++ scripts/smart-ticketing.js
@@ -1,11 +1,26 @@
-// function selectASeat()
-function removeBackgroundColorById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.remove('bg-[#F7F7F7');
+
+function findSeat(targetSeat) {
+    // const selectedSeat = document.getElementById(targetSeat);
+    // console.log()
+    const seats = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4', 'I1', 'I2', 'I3', 'I4', 'J1', 'J2', 'J3', 'J4'];
+    if (seats.includes(targetSeat)) {
+        console.log(targetSeat.classList)
+    }
+    else {
+        console.log('not found', targetSeat)
+    }
+
 }
 
 
-// Setting Background color to the seat button
+// Removing Background color from the seat button
+// function removeBackgroundColorById(elementId) {
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('bg-[#F7F7F7');
+// }
+
+
+// Set and Remove Background color to the seat button
 function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('bg-[#F7F8F8]');
@@ -42,44 +57,77 @@ function createTableRowInPurchaseForm(selectedSeat) {
 }
 
 
-document.getElementById('seat-container').addEventListener('click', handleKeyboardKeyUpEvent);
 
-function handleKeyboardKeyUpEvent(evn) {
-    const userClicked = evn.target;
-    userClickedSeatId = userClicked.id;
-    // console.log(userClickedSeatId);
-    setBackgroundColorById(userClickedSeatId);
+// function updateTotal(){
+//     const 
+// }
 
-    const selectedSeatText = document.getElementById(userClickedSeatId);
-    const selectedSeat = selectedSeatText.innerText;
-    console.log(selectedSeat)
-
-    // const
-
-
-
-    updateTotalSeat();
-    createTableRowInPurchaseForm(selectedSeat);
-
-
-
-
+// Update Selected Seat Count
+function updateSelectedSeatCountById(elementId) {
+    let selectedSeatCountElement = document.getElementById('selected-seat');
+    const selectedSeatCountText = parseInt(selectedSeatCountElement.innerText);
+    selectedSeatCountElement.innerText = elementId;
+    return elementId;
 }
 
 
 
 
 
+// ============================ New Function Starting here ============================
 
-function findSeat(targetSeat) {
-    // const selectedSeat = document.getElementById(targetSeat);
-    // console.log()
-    const seats = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4', 'I1', 'I2', 'I3', 'I4', 'J1', 'J2', 'J3', 'J4'];
-    if (seats.includes(targetSeat)) {
-        console.log(targetSeat.classList)
-    }
-    else {
-        console.log('not found', targetSeat)
-    }
+let seatList = document.getElementsByClassName('sit-button');
+// console.log(seatList)
 
+let selectedSeats = [];
+let seatCounter = 0;
+let totalPrice = 0;
+// const ticketPrice = 550;
+
+
+for (const seat of seatList) {
+    // console.log(seat.className)
+    seat.addEventListener('click', function (seatEvent) {
+        console.log(seatEvent)
+        // seatCounter = seatCounter + 1;
+
+        // console.log("clicked btn")
+        if (seatCounter < 4) {
+            if (selectedSeats.includes(seat)) {
+                alert("You can't select a seat multiple time;")
+                return;
+            }
+            else {
+                seatCounter = seatCounter + 1;
+                console.log("clicked btn", seat);
+                updateTotalSeat();
+                setBackgroundColorById(seat.id);
+                createTableRowInPurchaseForm(seat.id);
+
+                updateSelectedSeatCountById(seatCounter);
+
+
+                // getting Total Price element
+                const totalPriceElement = document.getElementById('total-price');
+                const totalPriceText = parseInt(totalPriceElement.innerText);
+                const totalPrice = seatCounter * 550;
+
+                totalPriceElement.innerText = totalPrice;
+
+                console.log(totalPrice)
+
+            }
+        }
+        else {
+            alert("You can not select more than 4 seats.")
+            return;
+        }
+        selectedSeats.push(seat);
+        console.log(selectedSeats)
+    })
 }
+
+
+
+
+
